ba279478716d6a75eb48880995582aec16ef8bd8
CNieblesR <camila.niebles@rootstrap.com>
2022-07-11 17:19:41 -0500

    Initial configuration

---
+++ scripts/gen.sh
@@ -0,0 +1,17 @@
+# USAGE: gen.sh PATH(s)
+# PATH: project path which contains a proto directory and
+#   a .proto named after the project (eg: blog, blog.proto)
+#   and optionally some other .proto files
+
+argc=$#
+argv=("$@")
+
+for (( j = 0; j < argc; ++j )); do
+  ./node_modules/.bin/grpc_tools_node_protoc -I  proto/                                   \
+    --js_out=import_style=commonjs: proto/                            \
+    --grpc_out=grpc_js: proto/                                        \
+    proto/**.proto;
+  ./node_modules/.bin/grpc_tools_node_protoc -I proto/                                   \
+    --js_out=import_style=commonjs:proto/                            \
+    $(find proto/ -type f -name "*.proto" -not -path "proto/**.proto")
+done
